d28e2f30bab70dc6c7adfc178b4fe525f8392128
gaowei <gaowei2016@yeah.net>
2019-12-17 14:35:23 +0800

    add home page

---
+++ lib/app.dart
@@ -0,0 +1,24 @@
+import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/material.dart';
+
+import 'entrance/page.dart';
+
+Widget createApp() {
+  final AbstractRoutes routes =
+      PageRoutes(pages: <String, Page<Object, dynamic>>{
+    'entracne_page': EntrancePage(),
+  });
+
+  return MaterialApp(
+    title: 'FishDemo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: routes.buildPage('entracne_page', null),
+    onGenerateRoute: (RouteSettings settings) {
+      return MaterialPageRoute<Object>(builder: (BuildContext context) {
+        return routes.buildPage(settings.name, settings.arguments);
+      });
+    },
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_listview_test/app.dart';
+
+void main() => runApp(createApp());
